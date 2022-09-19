f0e5da4cc26572e637b96f1e925eb05e696b2777
CrMJuarez <cmanueljuarez1@gmail.com>
2022-09-19 11:19:38 -0500

    Reestructutacion

---
+++ BL/DatosPortal.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class DatosPortal
+    {
+        public static ML.Result Add(ML.DatosPortal datosPortal)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string queryyy = "DatosAdd";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = queryyy;
+                    cmd.Connection = context;
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter[] collection = new SqlParameter[13];
+
+                    collection[0] = new SqlParameter("@IdFolioDeServicio", System.Data.SqlDbType.VarChar);
+                    collection[0].Value = datosPortal.IdFolioDeServicio;
+
+                    collection[1] = new SqlParameter("@IdFolioDeServicio", System.Data.SqlDbType.VarChar);
+                    collection[1].Value = datosPortal.Prioridad;
+
+                    collection[2] = new SqlParameter("@TipoServicio", System.Data.SqlDbType.VarChar);
+                    collection[2].Value = datosPortal.TipoServicio;
+
+                    collection[3] = new SqlParameter("@SucursalConsignatario", System.Data.SqlDbType.VarChar);
+                    collection[3].Value = datosPortal.SucursalConsignatario;
+
+                    collection[4] = new SqlParameter("@FechaCaptura", System.Data.SqlDbType.VarChar);
+                    collection[4].Value = datosPortal.FechaCaptura;
+
+                    collection[5] = new SqlParameter("@FechaRealizarServicio", System.Data.SqlDbType.VarChar);
+                    collection[5].Value = datosPortal.FechaRealizarServicio;
+
+                    collection[6] = new SqlParameter("@OrdenServicio", System.Data.SqlDbType.VarChar);
+                    collection[6].Value = datosPortal.OrdenServicio;
+
+                    collection[7] = new SqlParameter("@Importe", System.Data.SqlDbType.Decimal);
+                    collection[7].Value = datosPortal.Importe;
+
+                    collection[8] = new SqlParameter("@Divisa", System.Data.SqlDbType.VarChar);
+                    collection[8].Value = datosPortal.Divisa;
+
+                    collection[9] = new SqlParameter("@Ter", System.Data.SqlDbType.VarChar);
+                    collection[9].Value = datosPortal.Ter;
+
+                    collection[10] = new SqlParameter("@HoraEnvio", System.Data.SqlDbType.VarChar);
+                    collection[10].Value = datosPortal.HoraEnvio;
+
+                    collection[11] = new SqlParameter("@Actualización", System.Data.SqlDbType.VarChar);
+                    collection[11].Value = datosPortal.Actualización;
+
+                    collection[12] = new SqlParameter("@Estatus", System.Data.SqlDbType.VarChar);
+                    collection[12].Value = datosPortal.Estatus;
+
+                    cmd.Parameters.AddRange(collection);
+                    cmd.Connection.Open();
+                    int RowsAffected = cmd.ExecuteNonQuery();
+                    if (RowsAffected > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+            }
+            return result;
+
+        }
+
+        public static ML.Result GetById(ML.DatosPortal datosPortal)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "GetByIdDatos";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        ML.DatosPortal datos1 = new ML.DatosPortal();
+                        SqlParameter[] collection = new SqlParameter[1];
+
+                        collection[0] = new SqlParameter("@IdFolioDeServicio", System.Data.SqlDbType.VarChar);
+                        collection[0].Value = datos1.IdFolioDeServicio;
+
+                        cmd.Parameters.AddRange(collection);
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+
+                            DataTable datosTable = new DataTable();
+                            da.Fill(datosTable);
+                            cmd.Connection.Open();
+
+                            if (datosTable.Rows.Count > 0)
+                            {
+
+                                DataRow row1 = datosTable.Rows[0];
+
+                                datosPortal.IdFolioDeServicio = row1[0].ToString();
+                                datosPortal.Prioridad = row1[1].ToString();
+                                datosPortal.TipoServicio = row1[2].ToString();
+                                datosPortal.SucursalConsignatario = row1[3].ToString();
+                                datosPortal.FechaCaptura = row1[4].ToString();
+                                datosPortal.FechaRealizarServicio = row1[5].ToString();
+                                datosPortal.OrdenServicio = row1[6].ToString();
+                                datosPortal.Importe = decimal.Parse(row1[7].ToString());
+                                datosPortal.Divisa = row1[8].ToString();
+                                datosPortal.Ter = row1[9].ToString();
+                                datosPortal.HoraEnvio = row1[10].ToString();
+                                datosPortal.Actualización = row1[11].ToString();
+                                datosPortal.Estatus = row1[12].ToString();
+                                result.Object = datosPortal;
+
+                                result.Correct = true;
+                            }
+                            else
+                            {
+                                result.Correct = false;
+                                result.ErrorMessage = "No se encontraron registros en la tabla Materia";
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+
+            }
+            return result;
+
+        }
+        public static ML.Result Update(ML.DatosPortal datosPortal)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string queryy = "DatosUpdate";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = queryy;
+                    cmd.Connection = context;
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter[] collection = new SqlParameter[13];
+
+                    collection[0] = new SqlParameter("@IdFolioDeServicio", System.Data.SqlDbType.VarChar);
+                    collection[0].Value = datosPortal.IdFolioDeServicio;
+
+                    collection[1] = new SqlParameter("@Prioridad", System.Data.SqlDbType.VarChar);
+                    collection[1].Value = datosPortal.Prioridad;
+
+                    collection[2] = new SqlParameter("@TipoServicio", System.Data.SqlDbType.VarChar);
+                    collection[2].Value = datosPortal.TipoServicio;
+
+                    collection[3] = new SqlParameter("@SucursalConsignatario", System.Data.SqlDbType.VarChar);
+                    collection[3].Value = datosPortal.SucursalConsignatario;
+
+                    collection[4] = new SqlParameter("@FechaCaptura", System.Data.SqlDbType.VarChar);
+                    collection[4].Value = datosPortal.FechaCaptura;
+
+                    collection[5] = new SqlParameter("@FechaRealizarServicio", System.Data.SqlDbType.VarChar);
+                    collection[5].Value = datosPortal.FechaRealizarServicio;
+
+                    collection[6] = new SqlParameter("@OrdenServicio", System.Data.SqlDbType.VarChar);
+                    collection[6].Value = datosPortal.OrdenServicio;
+
+                    collection[7] = new SqlParameter("@Importe", System.Data.SqlDbType.Decimal);
+                    collection[7].Value = datosPortal.Importe;
+
+                    collection[8] = new SqlParameter("@Divisa", System.Data.SqlDbType.VarChar);
+                    collection[8].Value = datosPortal.Divisa;
+
+                    collection[9] = new SqlParameter("@Ter", System.Data.SqlDbType.VarChar);
+                    collection[9].Value = datosPortal.Ter;
+
+                    collection[10] = new SqlParameter("@HoraEnvio", System.Data.SqlDbType.VarChar);
+                    collection[10].Value = datosPortal.HoraEnvio;
+
+                    collection[11] = new SqlParameter("@Actualización", System.Data.SqlDbType.VarChar);
+                    collection[11].Value = datosPortal.Actualización;
+
+                    collection[12] = new SqlParameter("@Estatus", System.Data.SqlDbType.VarChar);
+                    collection[12].Value = datosPortal.Estatus;
+
+                    cmd.Parameters.AddRange(collection);
+                    cmd.Connection.Open();
+                    int RowsAffected = cmd.ExecuteNonQuery();
+                    if (RowsAffected > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+
+            }
+            return result;
+
+        }
+    }
+}
+
+
+
--- BL/Properties/AssemblyInfo.cs
+++ BL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("3695baa7-f9b4-4a95-bed9-5e245282425e")]
+[assembly: Guid("6252e6eb-3cb9-4e2b-92f0-f62927df0e94")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
-// mediante el carácter '*', como se muestra a continuación:
+// Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
+// mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
