252d7f11fcb1b970cb0738bba8570f5a48311b49
CrMJuarez <cmanueljuarez1@gmail.com>
2022-09-27 00:45:16 -0500

    Actualizaciones

---
+++ PL/DatosPortal.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Data;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support;
+namespace PL
+
+{
+
+    public class DatosPortal
+    {
+        //    void Page_Load(Object sender, EventArgs e)
+        //    {
+
+        //        Button1.Click += new EventHandler(this.Btn_Click);
+        //    }
+
+        //    void Btn_Click(Object sender,
+        //                           EventArgs e)
+        //    {
+
+        //        Button clickedButton = (Button)sender;
+
+        //        clickedButton.Enabled = false;
+
+        //    }
+
+
+        public static void ExtraerDatos()
+
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://portal.gsi.com.mx:8443/portal_desa/Logout.do");
+
+            var Input = driver.FindElement(By.Id("txtUsuario"));
+            Input.SendKeys("MONHDRS03");
+
+            var Input1 = driver.FindElement(By.Id("txtPassword"));
+            Input1.SendKeys("123");
+
+            var Input2 = driver.FindElement(By.Name("imgLogin"));
+            Input2.Submit();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.FindElement(By.XPath("//select[@id='" + "cboMenus" + "']/option[contains(.,'" + "Monitor de Solicitudes TV" + "')]")).Click();
+
+            //var step2 = (driver.FindElements(By.XPath("//*[contains(@id, 'dropdown-intervaltype')]")));
+            //select.select_by_value("Previous Year")
+
+
+            driver.Navigate().GoToUrl("https://portal.gsi.com.mx:8443/portal_desa/Login.do");
+
+
+
+
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            //wait.Until(SeleniumExtras.WaitHelpers.Expect
+            //edConditions.ElementIsVisible(By.ClassName("combo")));
+
+            //            var Input3 = driver.FindElement(By.Id("cboMenus"));
+
+
+            //            Input3.Submit();
+
+
+
+
+
+
+
+
+
+
+
+            // driver.FindElement(By.Id("btnFillFecha"));
+
+
+
+
+
+
+
+            // var Password = ConfigurationManager.AppSettings["txtPassword"];
+
+            //string formParams = string.Format("txtUsuario={0}&txtPassword={1}", "MONHDRS03", "123");
+            //string cookieHeader;
+
+            //WebRequest request = WebRequest.Create("https://portal.gsi.com.mx:8443/portal_desa/Logout.do");
+            //request.ContentType = "text/plain";
+            //request.Method = "POST";
+            //byte[] bytes = Encoding.ASCII.GetBytes(formParams);
+            //request.ContentLength = bytes.Length;
+            //using (Stream os = request.GetRequestStream())
+            //{
+            //    os.Write(bytes, 0, bytes.Length);
+            //}
+            //WebResponse response = request.GetResponse();
+            //cookieHeader = response.Headers["Set-Cookie"];
+
+            //WebRequest getRequest = WebRequest.Create("https://portal.gsi.com.mx:8443/portal_desa/Inicio.do?pUsuClave=15142&pUsuRolClave=41&pUsuNombre=MONITOREO%20HIDROSINA%2003&pUsuBanClave=1&pUsuSubClave=19102&pUsuSucClave=%202&pUsuTraClave=62&pNueUsu=&pEmpClave=&pEmpDesc=&claveTraslado=%201&pUsuRegClave=&pUsuMultiBanco=1&pUsuIdSucursalBovirBpf=");
+            //getRequest.Method = "GET";
+            //getRequest.Headers.Add("Cookie", cookieHeader);
+            //WebResponse getResponse = getRequest.GetResponse();
+
+
+
+
+            //WebRequest getRequest2 = WebRequest.Create("https://portal.gsi.com.mx:8443/portal_desa/MonitorServicios.do?monitorRefresh=&pPage=&pUsuRolClave=41&hdnObjectValues=&hdnOperation=F&hdnResultado=&hdnSucursal=0&hdnBanco=0&hdnEstatus=0&hdnTipoFecha=A+Realizar+Servicio&hdnTipoDeposito=&hdnRegion=0&hdnTipoOrden=0&hdnFolio=&hdnFechaDel=22%2F09%2F2022&hdnFechaAl=22%2F09%2F2022&hdnBusqueda=&pUsuMultiBanco=1&horaEnvio=&hndNomenclatura=Todos&pUsuClave=15142&pUsuRolClave=41&pUsuBanClave=1&pUsuSubClave=19102&pUsuNombre=&pUsuSucClave=+2&pUsuTraClave=62&pEmpClave=&pEmpDesc=");
+            //getRequest.Method = "POST";
+            //getRequest.Headers.Add("Cookie", cookieHeader);
+            //WebResponse getResponse2 = getRequest.GetResponse();
+
+            /////////////////////////////
+
+
+            //WebBrowser webBrowser = new WebBrowser();
+            //webBrowser.Navigate("https://portal.gsi.com.mx:8443/portal_desa/MonitorServicios.do?carga=1&pUsuClave=15142&pUsuRolClave=41&pUsuBanClave=1&pUsuSubClave=19102&pUsuSucClave=%202&pUsuTraClave=62&pEmpClave=&pEmpDesc=&pUsuMultiBanco=1&pUsuIdSucursalBovirBpf=&claveTraslado=%201");
+            //webBrowser.Document.GetElementById("btnFilFecha").InvokeMember("click");
+
+
+
+
+
+
+
+            //var doc = new HtmlDocument();
+            //var request = (HttpWebRequest)WebRequest.Create(url);
+            //request.Method = "GET";
+            //using (var response = (HttpWebResponse)request.GetResponse())
+            //{
+            //    using (var stream = response.GetResponseStream())
+            //    {
+            //        doc.Load(stream);
+            //    }
+            //}
+            //var table = doc.GetElementbyId("tblThreads");
+
+
+
+            ////////////////////////
+            WebClient webClient = new WebClient();
+
+            string page = webClient.DownloadString("https://datosprueba01.000webhostapp.com/DatosPreba02/Datos.html");
+            //// "https://portal.gsi.com.mx:8443/portal_desa/MonitorServicios.do?monitorRefresh=&pPage=&pUsuRolClave=41&hdnObjectValues=&hdnOperation=F&hdnResultado=&hdnSucursal=0&hdnBanco=0&hdnEstatus=0&hdnTipoFecha=A+Realizar+Servicio&hdnTipoDeposito=&hdnRegion=0&hdnTipoOrden=0&hdnFolio=&hdnFechaDel=22%2F09%2F2022&hdnFechaAl=22%2F09%2F2022&hdnBusqueda=&pUsuMultiBanco=1&horaEnvio=&hndNomenclatura=Todos&pUsuClave=15142&pUsuRolClave=41&pUsuBanClave=1&pUsuSubClave=19102&pUsuNombre=&pUsuSucClave=+2&pUsuTraClave=62&pEmpClave=&pEmpDesc="
+            ////monitorRefresh=&pPage=&pUsuRolClave=41&hdnObjectValues=&hdnOperation=F&hdnResultado=&hdnSucursal=0&hdnBanco=0&hdnEstatus=0&hdnTipoFecha=A+Realizar+Servicio&hdnTipoDeposito=&hdnRegion=0&hdnTipoOrden=0&hdnFolio=&hdnFechaDel=22%2F09%2F2022&hdnFechaAl=22%2F09%2F2022&hdnBusqueda=&pUsuMultiBanco=1&horaEnvio=&hndNomenclatura=Todos&pUsuClave=15142&pUsuRolClave=41&pUsuBanClave=1&pUsuSubClave=19102&pUsuNombre=&pUsuSucClave=+2&pUsuTraClave=62&pEmpClave=&pEmpDesc=
+
+            ////https://portal.gsi.com.mx:8443/portal_desa/MonitorServicios.do
+            ////carga el documento html del portal y lo guarda en doc
+            var doc = new HtmlDocument();
+
+            doc.LoadHtml(page);
+
+            //var databutton = doc.GetElementbyId("btnFilFecha");
+
+            //var databutton = doc.GetElementbyId("btnFilFecha");
+            //databutton.
+
+
+
+
+            //var el = doc.do.GetElementById("email");
+            //if (el != null)
+            //    el.InvokeMember("click");
+
+
+
+
+            //lee la tabla que encontro en el documento por el id de la tabla y lo muestra por div
+            var myTable = doc.DocumentNode
+                 .Descendants("div")
+                 .Where(t => t.Attributes["id"].Value == "tablaJson")
+                 .FirstOrDefault();
+
+            //se hace un foreach para que selecciones los datos de la tabla
+            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table[1]"))
+            {
+                //segundo foreach para que divida el documento por div
+                foreach (HtmlNode row in table.SelectNodes("//div"))
+                {
+                    //  tercer foreach para dividir el documento pot tr
+                    foreach (HtmlNode cell in row.SelectNodes("//tr"))
+                    {
+                        //trae los td de forma decendiente y en un arreglo
+                        var tds = cell.Descendants("td").ToArray(); // trae todos los td
+
+                        if (tds.Count() == 19)
+                        {
+                            var Valor = "Id";
+
+                            if (tds[0].InnerHtml.Equals(Valor))
+                            {
+                                continue;
+                            }
+                        }
+
+                        if (tds.Count() == 19)
+                        {
+                            //guarda los datos en las variables correspondientes para agregar a la base de datos
+
+                            ML.DatosPortal datosPortal = new ML.DatosPortal();
+
+                            datosPortal.Prioridad = tds[1].InnerText.ToString();
+                            datosPortal.TipoServicio = tds[2].InnerText.ToString();
+                            datosPortal.SucursalConsignatario = tds[3].InnerText.ToString();
+                            datosPortal.FechaCaptura = tds[4].InnerText.ToString();
+                            datosPortal.FechaRealizarServicio = tds[5].InnerText.ToString();
+                            datosPortal.IdFolioDeServicio = tds[6].InnerText.ToString();
+                            datosPortal.OrdenServicio = tds[7].InnerText.ToString();
+                            char[] chars = { ' ' };
+                            string Imp = tds[8].InnerText;
+                            string Import = Imp.Trim(chars);
+                            datosPortal.Importe = decimal.Parse(Import.ToString());
+                            datosPortal.Divisa = tds[9].InnerText.ToString();
+                            datosPortal.Te = tds[10].InnerText.ToString();
+                            datosPortal.HoraEnvio = tds[11].InnerText.ToString();
+                            datosPortal.Actualización = tds[12].InnerText.ToString();
+                            datosPortal.Estatus = tds[13].InnerText.ToString();
+
+                            if (datosPortal.IdFolioDeServicio == null)
+                            {
+                                Console.WriteLine("No existe formato valido de folio de servicio");
+                            }
+                            else
+                            {
+                                ML.Result result = BL.DatosPortal.GetById(datosPortal.IdFolioDeServicio);
+                                if (result.Correct)
+                                {
+                                    BL.DatosPortal.Update(datosPortal);
+                                    Console.WriteLine("Se modificaron los datos");
+                                }
+                                else
+                                {
+                                    BL.DatosPortal.Add(datosPortal);
+                                    Console.WriteLine("Se registraron los datos");
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ PL/Sesion.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Configuration;
+using System.Web.UI;
+namespace PL
+{
+    public class Sesion
+    {
+        //private static Sesion instance;
+        //private Sesion()
+        //{
+
+        //}
+
+        //private static readonly object locker = new object();
+        //public static Sesion GetInstance()
+        //{
+        //    lock (locker)
+        //    {
+        //        if (instance == null)
+        //        {
+        //            instance = new Sesion();
+        //        }
+        //        return instance;
+        //    }
+        //}
+        //    /// <summary>
+        //    /// Obtener datos del servidor a través de Get
+        //    /// </summary>
+        //    /// <param name = "url"> dirección de solicitud </param>
+        //    /// <param name = "timeout"> Periodo de tiempo de espera </param>
+        //    /// <param name = "statusCode"> Código de respuesta personalizado </param>
+        //    /// <param name = "message"> información de respuesta </param>
+        //    /// <returns></returns>
+        //    public string GetRequest(string url, int timeout, out int statusCode, out string message)
+        //    {
+        //        string getUrl = VerifyUrl(url);
+        //        if (getUrl.StartsWith("https://portal.gsi.com.mx:8443/portal_desa/Login.do"))
+        //        {
+        //            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidate;
+        //            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+        //        }
+        //        try
+        //        {
+        //            Encoding encoding = Encoding.UTF8;
+        //            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(new Uri(getUrl));
+        //            // Cancela el proxy para evitar un acceso lento por primera vez
+        //            webRequest.Proxy = null;
+        //            webRequest.KeepAlive = true;
+        //            webRequest.Method = "Get";
+        //            webRequest.Timeout = timeout;
+        //            SetHeaderValue(webRequest.Headers, "Accept", "application/json");
+        //            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+        //            StreamReader sr = new StreamReader(response.GetResponseStream(), encoding);
+        //            string result = sr.ReadToEnd();
+        //            statusCode = (int)response.StatusCode;
+        //            message = "";
+        //            return result;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            statusCode = -1;
+        //            message = ex.Message;
+        //            return string.Empty;
+        //            throw;
+        //        }
+        //    }
+
+        //    /// <summary>
+        //    /// Inicie una solicitud de registro al servidor a través de Post.
+        //    /// </summary>
+        //    /// <param name="url"></param>
+        //    /// <param name="timeout"></param>
+        //    /// <param name="parameters"></param>
+        //    /// <param name="statusCode"></param>
+        //    /// <param name="message"></param>
+        //    /// <returns></returns>
+        //    public string PostRequestRegister(string url, int timeout, IDictionary<string, string> parameters, out int statusCode, out string message)
+        //    {
+        //        string getUrl = VerifyUrl(url);
+        //        if (getUrl.StartsWith("https://https://portal.gsi.com.mx:8443/portal_desa/Login.do"))
+        //        {
+        //            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidate;
+        //            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+        //        }
+        //        try
+        //        {
+        //            Encoding encoding = Encoding.UTF8;
+        //            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(new Uri(getUrl));
+        //            webRequest.Proxy = null;
+        //            webRequest.KeepAlive = true;
+        //            webRequest.Method = "Post";
+        //            webRequest.Timeout = timeout;
+        //            SetHeaderValue(webRequest.Headers, "Content-Type", "appliction/x-www-form-urlencoded");
+        //            SetHeaderValue(webRequest.Headers, "Accept", "application/json");
+        //            if (parameters != null && parameters.Count != 0)
+        //            {
+        //                StringBuilder buffer = new StringBuilder();
+        //                bool first = true;
+        //                foreach (string key in parameters.Keys)
+        //                {
+        //                    if (!first)
+        //                    {
+        //                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+        //                    }
+        //                    else
+        //                    {
+        //                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+        //                        first = false;
+        //                    }
+        //                }
+        //                byte[] json = Encoding.UTF8.GetBytes(buffer.ToString());
+        //                // Escribir flujo de solicitud
+        //                using (Stream stream = webRequest.GetRequestStream())
+        //                {
+        //                    stream.Write(json, 0, json.Length);
+        //                }
+        //            }
+
+        //            HttpWebResponse response = (webRequest.GetResponse() as HttpWebResponse);
+        //            statusCode = (int)response.StatusCode;
+        //            string result;
+        //            using (StreamReader sr = new StreamReader(response.GetResponseStream(), encoding))
+        //            {
+        //                result = sr.ReadToEnd();
+        //            }
+        //            message = "";
+        //            return result;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            statusCode = -1;
+        //            message = ex.Message;
+        //            return string.Empty;
+        //        }
+        //    }
+
+        //    /// <summary>
+        //    /// Publicar solicitud para iniciar sesión (los parámetros de solicitud aquí son diferentes a los del momento del registro)
+        //    /// </summary>
+        //    /// <param name="url"></param>
+        //    /// <param name="timeout"></param>
+        //    /// <param name="parameters"></param>
+        //    /// <param name="statusCode"></param>
+        //    /// <param name="message"></param>
+        //    /// <returns></returns>
+        //    public string PostRequestLogin(string url, int timeout, IDictionary<string, string> parameters, out int statusCode, out string message)
+        //    {
+        //        string getUrl = VerifyUrl(url);
+        //        if (getUrl.StartsWith("https://https://portal.gsi.com.mx:8443/portal_desa/Login.do"))
+        //        {
+        //            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidate;
+        //            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+        //        }
+        //        try
+        //        {
+        //            Encoding encoding = Encoding.UTF8;
+        //            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(new Uri(getUrl));
+        //            webRequest.Proxy = null;
+        //            webRequest.Timeout = timeout;
+        //            webRequest.KeepAlive = true;
+        //            webRequest.Method = "Post";
+        //            SetHeaderValue(webRequest.Headers, "Content-Type", "application/json");
+        //            if (parameters != null && parameters.Count != 0)
+        //            {
+        //                StringBuilder buffer = new StringBuilder();
+        //                bool first = true;
+        //                foreach (string key in parameters.Keys)
+        //                {
+        //                    if (!first)
+        //                    {
+        //                        buffer.AppendFormat(",\"" + "{0}" + "\":\"" + "{1}" + "\"}}", key, parameters[key]);
+        //                    }
+        //                    else
+        //                    {
+        //                        buffer.AppendFormat("{{" + "\"" + "{0}" + "\":\"" + "{1}" + "\"", key, parameters[key]);
+        //                    }
+        //                }
+        //                byte[] json = Encoding.UTF8.GetBytes(buffer.ToString());
+        //                // Escribir flujo de solicitud
+        //                using (Stream stream = webRequest.GetRequestStream())
+        //                {
+        //                    stream.Write(json, 0, json.Length);
+        //                }
+        //            }
+        //            HttpWebResponse response = (webRequest.GetResponse() as HttpWebResponse);
+        //            statusCode = (int)response.StatusCode;
+        //            string result;
+        //            using (StreamReader sr = new StreamReader(response.GetResponseStream(), encoding))
+        //            {
+        //                result = sr.ReadToEnd();
+        //            }
+        //            message = "";
+        //            return result;
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            statusCode = -1;
+        //            message = ex.Message;
+        //            return string.Empty;
+        //        }
+        //    }
+        //    /// <summary>
+        //    /// Agregar manualmente el encabezado de la solicitud
+        //    /// </summary>
+        //    /// <param name="headers"></param>
+        //    /// <param name="name"></param>
+        //    /// <param name="value"></param>
+        //    private void SetHeaderValue(WebHeaderCollection headers, string name, string value)
+        //    {
+        //        var property = typeof(WebHeaderCollection).GetProperty("InnerCollection", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+        //        if (property != null)
+        //        {
+        //            var collection = property.GetValue(headers, null) as System.Collections.Specialized.NameValueCollection;
+        //            collection[name] = value;
+        //        }
+        //    }
+
+        //    public string VerifyUrl(string url)
+        //    {
+        //        if (string.IsNullOrEmpty(url))
+        //        {
+        //            //anzar una nueva excepción("¡La URL no puede estar vacía!");
+        //        }
+        //        if (url.StartsWith("https://portal.gsi.com.mx:8443/portal_desa/Login.do", StringComparison.CurrentCultureIgnoreCase) || url.StartsWith("https://", StringComparison.CurrentCultureIgnoreCase))
+        //        {
+        //            return url;
+        //        }
+        //        else
+        //        {
+        //            // lanzar una nueva excepción("¡Ingrese la URL correcta!");
+        //            return string.Format("https://portal.gsi.com.mx:8443/portal_desa/Login.do", url);
+        //        }
+        //    }
+
+        //    // Confía en cualquier certificado
+        //    public static bool RemoteCertificateValidate(object sender, X509Certificate cert, X509Chain chain, SslPolicyErrors error)
+        //    {
+        //        return true;
+        //    }
+
+        //    /// <summary>
+        //    /// Verificar conexión
+        //    /// </summary>
+        //    /// <param name="url"></param>
+        //    /// <returns></returns>
+        //    public bool IsWebRequest(string url)
+        //    {
+        //        if (url.StartsWith("https://portal.gsi.com.mx:8443/portal_desa/Login.do"))
+        //        {
+        //            ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidate;
+        //            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+        //        }
+        //        try
+        //        {
+        //            WebRequest webRequest = WebRequest.Create(url);
+        //            webRequest.Proxy = null;
+        //            webRequest.Timeout = 20000;
+        //            WebResponse webResponse = webRequest.GetResponse();
+        //            webResponse.Close();
+        //            return true;
+        //        }
+        //        catch (Exception)
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}  
+        public static void Session()
+
+        {
+            //var Password = ConfigurationManager.AppSettings["txtPassword"];
+
+            string formParams = string.Format("txtUsuario={0}&txtPassword={1}", "MONHDRS03", "123");
+            string cookieHeader;
+
+            WebRequest request = WebRequest.Create("https://portal.gsi.com.mx:8443/portal_desa/Logout.do");
+            request.ContentType = "text/plain";
+            request.Method = "POST";
+            byte[] bytes = Encoding.ASCII.GetBytes(formParams);
+            request.ContentLength = bytes.Length;
+            using (Stream os = request.GetRequestStream())
+            {
+                os.Write(bytes, 0, bytes.Length);
+            }
+            WebResponse response = request.GetResponse();
+            cookieHeader = response.Headers["Set-Cookie"];
+
+            WebRequest getRequest = WebRequest.Create("https://portal.gsi.com.mx:8443/portal_desa/Inicio.do?pUsuClave=15142&pUsuRolClave=41&pUsuNombre=MONITOREO%20HIDROSINA%2003&pUsuBanClave=1&pUsuSubClave=19102&pUsuSucClave=%202&pUsuTraClave=62&pNueUsu=&pEmpClave=&pEmpDesc=&claveTraslado=%201&pUsuRegClave=&pUsuMultiBanco=1&pUsuIdSucursalBovirBpf=");
+            getRequest.Method = "GET";
+            getRequest.Headers.Add("Cookie", cookieHeader);
+            WebResponse getResponse = getRequest.GetResponse();
+
+            WebRequest getRequest2 = WebRequest.Create("https://portal.gsi.com.mx:8443/portal_desa/MonitorServicios.do?monitorRefresh=&pPage=&pUsuRolClave=41&hdnObjectValues=&hdnOperation=F&hdnResultado=&hdnSucursal=0&hdnBanco=0&hdnEstatus=0&hdnTipoFecha=A+Realizar+Servicio&hdnTipoDeposito=&hdnRegion=0&hdnTipoOrden=0&hdnFolio=&hdnFechaDel=22%2F09%2F2022&hdnFechaAl=22%2F09%2F2022&hdnBusqueda=&pUsuMultiBanco=1&horaEnvio=&hndNomenclatura=Todos&pUsuClave=15142&pUsuRolClave=41&pUsuBanClave=1&pUsuSubClave=19102&pUsuNombre=&pUsuSucClave=+2&pUsuTraClave=62&pEmpClave=&pEmpDesc=");
+            getRequest.Method = "POST";
+            getRequest.Headers.Add("Cookie", cookieHeader);
+            WebResponse getResponse2 = getRequest.GetResponse();
+
+
+
+        }
+    }
+}
